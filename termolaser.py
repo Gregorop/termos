914686fe268f85e87888bd544bcd004cad964ec9
gregor <gregorop@yandex.ru>
2023-04-10 10:52:25 +0300

    есть выбор области, чтение файла и сохранение

--- termolaser.py
+++ termolaser.py
@@ -5,10 +5,15 @@ def shoot(_surface):
     surface_array = surfarray.array2d(_surface)
     for x in range(len(surface_array)):
         for y in range(len(surface_array[x])):
-            
-            if sum(_surface.unmap_rgb(surface_array[x][y])[:-1]) > 100:
-                _surface.set_at((x,y), (255,0,0))
-   
+            value = sum(_surface.unmap_rgb(surface_array[x][y])[:-1])
+            kef = value/3
+            if value > 130:
+                _surface.set_at((x,y), (kef,kef/5,0))
+            elif value < 80:
+                _surface.set_at((x,y), (0,0,255))
+            else:
+                _surface.set_at((x,y), (kef/10,kef,0))
+    
 
 if __name__ == "__main__":
     img = image.load('2.jpg')
--- test_stand.py
+++ test_stand.py
@@ -1,17 +1,42 @@
 from pygame import *
 from termolaser import *
 
-w,h = 640, 480
+from PyQt5 import QtWidgets
+
+from PIL import Image
+
+def field_move_border():
+    if field.left < 0: field.left = 0
+    if field.right > w: field.right = w
+    if field.bottom > h: field.bottom = h
+    if field.top < 0: field.top = 0
+
+
+app = QtWidgets.QApplication([])
+wb_patch = QtWidgets.QFileDialog.getOpenFileName()[0]
+
+print(wb_patch)
+w,h = Image.open(wb_patch).size
 
 win = display.set_mode([w,h])
-img = image.load('2.jpg')
-area = img.subsurface(w//2-150,h//2-150,300,300)
-shoot(area)
+img = image.load(wb_patch)
+field = Rect(0,0,w//2,h//2)
 
 while True:
+    
     for e in event.get():
         if e.type == QUIT: exit()
 
-        
+        if e.type == MOUSEMOTION:
+            field.center = e.pos
+            field_move_border()
+
+        if e.type == MOUSEBUTTONDOWN:
+            area = img.subsurface(field)
+            shoot(area)
+            
+            image.save(img, 'final'+wb_patch.split("/")[-1])
+            
     win.blit(img,(0,0))
+    draw.rect(win,(0,0,0),field,3)
     display.update()
